75a35455e4729e2f5f11d4e578affbbd52512b5a
hezhaoyun <hezhaoyun@outlook.com>
2023-04-12 19:36:42 +0800

    use pikafish flutter plugin

--- app/lib/engine/pikafish_engine.dart
+++ app/lib/engine/pikafish_engine.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:pikafish_engine/pikafish.dart';
+import 'package:pikafish_engine/pikafish_engine.dart';
 
 import '../cchess/position.dart';
 import '../common/prt.dart';
